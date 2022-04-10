cc89004253b921a3023ab286728dfbbbb1420fe3
mujtaba <mujtabaibrar369@gmail.com>
2022-04-10 16:21:55 +0500

    add searchbar to usermanagement

--- User Management System/script.js
+++ User Management System/script.js
@@ -1,35 +1,44 @@
-let record = new Array();
 let errorPopup = document.querySelector('.popup');
 const overlay = document.querySelector('.overlay');
-console.log(popup);
+let row = 1;
+let display = document.getElementById('customers');
 document.getElementById('button').addEventListener('click', function () {
-  document.getElementById('records').textContent = '';
   var username = document.getElementById('username').value;
   var age = document.getElementById('age').value;
   if (username == '' || age == '') {
     overlay.classList.remove('hidden');
     errorPopup.classList.remove('hidden');
   } else {
+    let newRow = display.insertRow(row);
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+    cell1.innerHTML = username;
+    cell2.innerHTML = age;
+    row++;
     document.getElementById('username').value = '';
     document.getElementById('age').value = '';
-    let data = {
-      username: username,
-      age: age,
-    };
-    record.push(data);
-    let temp = document.getElementById('records');
-    for (let i = 0; i <= record.length; i++) {
-      temp.innerHTML +=
-        'Name : ' + record[i].username + '  Age : ' + record[i].age + '<br>';
-    }
   }
 });
 document.getElementById('popupbutton').addEventListener('click', function () {
   let temp = document.getElementById('records');
   errorPopup.classList.add('hidden');
   overlay.classList.add('hidden');
-  for (let i = 0; i <= record.length; i++) {
-    temp.innerHTML +=
-      'Name : ' + record[i].username + '  Age : ' + record[i].age + '<br>';
-  }
 });
+function myFunction() {
+  var input, filter, table, tr, td, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  table = document.getElementById('customers');
+  tr = table.getElementsByTagName('tr');
+  for (let i = 0; i < row; i++) {
+    td = tr[i].getElementsByTagName('td')[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = '';
+      } else {
+        tr[i].style.display = 'none';
+      }
+    }
+  }
+}
