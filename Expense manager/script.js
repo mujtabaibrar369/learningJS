fceab2cb1bed951687fb0cb73b01f41ef613a57e
mujtabaibrar369 <97233871+mujtabaibrar369@users.noreply.github.com>
2022-08-06 00:30:12 +0500

    commit

--- Expense manager/script.js
+++ Expense manager/script.js
@@ -8,6 +8,7 @@ let counterExpense = 1;
 let persons = [];
 let display = document.getElementById('customers');
 let expenseTable = document.getElementById('Expenses');
+let currentUserId = -1;
 document.getElementById('button').addEventListener('click', function () {
   let person = {
     id: Math.floor(Math.random() * 1000),
@@ -15,6 +16,7 @@ document.getElementById('button').addEventListener('click', function () {
     age: document.getElementById('age').value,
     income: document.getElementById('income').value,
     expenses: [],
+    totalExpenses: 0,
   };
   persons.push(person);
   if (person.username == '' || person.age == '' || person.income == '') {
@@ -27,12 +29,13 @@ document.getElementById('button').addEventListener('click', function () {
     let cell3 = newRow.insertCell(2);
     let cell4 = newRow.insertCell(3);
     let cell5 = newRow.insertCell(4);
+    cell5.id = `expense${persons[row - 1].id}`;
     let cell6 = newRow.insertCell(5);
     cell1.innerHTML = persons[row - 1].id;
     cell2.innerHTML = persons[row - 1].username;
     cell3.innerHTML = persons[row - 1].age;
     cell4.innerHTML = persons[row - 1].income;
-    cell5.innerHTML += '';
+    cell5.innerHTML += persons[row - 1].totalExpenses;
     cell6.innerHTML += `<input type='button' onclick = "addExpenses(this)" id='${
       persons[row - 1].id
     }' value='Add expense'>`;
@@ -40,6 +43,7 @@ document.getElementById('button').addEventListener('click', function () {
       persons[row - 1].id
     }' value='View Expense'>`;
     row++;
+    console.log(cell5.id);
     document.getElementById('name').value = '';
     document.getElementById('age').value = '';
     document.getElementById('income').value = '';
@@ -50,7 +54,6 @@ document.getElementById('popupbutton').addEventListener('click', function () {
   overlay.classList.add('hidden');
 });
 document.getElementById('adduser').addEventListener('click', function () {
-  console.log('test');
   viewData.classList.add('hidden');
   input.classList.remove('hidden');
 });
@@ -59,7 +62,6 @@ document.getElementById('back').addEventListener('click', function () {
   viewData.classList.remove('hidden');
 });
 document.getElementById('goback').addEventListener('click', function () {
-  console.log('test');
   addExpensePopup.classList.add('hidden');
   viewData.classList.remove('hidden');
 });
@@ -86,38 +88,61 @@ function addExpenses(e) {
   addExpensePopup.classList.remove('hidden');
   viewData.classList.add('hidden');
   input.classList.add('hidden');
-  document
-    .getElementById('addexpensebutton')
-    .addEventListener('click', function () {
-      let exptitle = document.getElementById('Expensetitle').value;
-      let expamount = document.getElementById('expenseamount').value;
-      let expdate = document.getElementById('expensedate').value;
-      if (exptitle == '' || expamount == '' || expdate == '') {
-        overlay.classList.remove('hidden');
-        errorPopup.classList.remove('hidden');
-      } else {
-        let Exp = {
-          expenseTitle: exptitle,
-          expenseAmount: expamount,
-          expenseDate: expdate,
-        };
-        for (let i = 0; i < row - 1; i++) {
-          if (persons[i].id == e.id) {
-            persons[i].expenses.push(Exp);
+  currentUserId = e.id;
+}
+document
+  .getElementById('addexpensebutton')
+  .addEventListener('click', function (e) {
+    let exptitle = document.getElementById('Expensetitle').value;
+    let expamount = document.getElementById('expenseamount').value;
+    let expdate = document.getElementById('expensedate').value;
+    if (exptitle == '' || expamount == '' || expdate == '') {
+      overlay.classList.remove('hidden');
+      errorPopup.classList.remove('hidden');
+    } else {
+      let Exp = {
+        expenseTitle: exptitle,
+        expenseAmount: expamount,
+        expenseDate: expdate,
+      };
+      for (let i = 0; i < row - 1; i++) {
+        if (persons[i].id == currentUserId) {
+          persons[i].expenses.push(Exp);
+          if (
+            persons[i].totalExpenses + Number(expamount) <
+            persons[i].income
+          ) {
+            persons[i].totalExpenses += Number(expamount);
+            console.log(persons[i].totalExpenses);
+            document.getElementById(`expense${currentUserId}`).innerHTML =
+              persons[i].totalExpenses;
+          } else if (
+            persons[i].totalExpenses + Number(expamount) ==
+            persons[i].income
+          ) {
+            let btn = document.getElementById(`${persons[i].id}`);
+            btn.disabled = true;
+            document.getElementById(`expense${currentUserId}`).innerHTML =
+              persons[i].totalExpenses;
+          } else {
+            overlay.classList.remove('hidden');
+            errorPopup.classList.remove('hidden');
           }
         }
-
-        counterExpense++;
-        document.getElementById('Expensetitle').value = '';
-        document.getElementById('expenseamount').value = '';
-        document.getElementById('expensedate').value = '';
       }
-      console.log('test');
-    });
-}
+
+      counterExpense++;
+      document.getElementById('Expensetitle').value = '';
+      document.getElementById('expenseamount').value = '';
+      document.getElementById('expensedate').value = '';
+    }
+  });
 function viewExpense(e) {
+  expenseTable.innerHTML =
+    '<tbody><tr><th>Title</td><th>Expesne Amount</td><th>Date</td></tr></tbody>';
+
   for (let i = 0; i < row - 1; i++) {
-    if (persons[i].id == e.id) {
+    if (persons[i].id == currentUserId) {
       let temp = persons[i].expenses;
       if (persons[i].expenses.length > 0) {
         for (let j = 0; j < persons[i].expenses.length; j++) {
